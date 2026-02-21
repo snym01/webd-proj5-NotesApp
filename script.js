4f305cd87f6dc1e2c84d1add12a285b2bc09107a
Sanyam <snym359@gmail.com>
2026-02-21 22:00:37 +0530

    dynamic note functionality added

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-const input = document.getElementById("title");
+const title = document.getElementById("title");
 const notePad = document.getElementById("note-pad");
 const addBtn = document.getElementById("addBtn");
-const NotesContainer = document.querySelector(".notes-container");
+const notesContainer = document.querySelector(".notes-container");
+
 let notes = []
 
 addBtn.addEventListener("click", function () {
-    if (notePad.value.trim() === "") return;
-    const noteObj = {
-        id: Date.now(),
-        title: input.value,
-        content: notePad.value
-    }
+    const trimmedTitle = title.value.trim();
+    const trimmedContent = notePad.value.trim();
+    if (trimmedContent === "") return;
+    const finalTitle = trimmedTitle || "Untitled";
 
-    notes.push(noteObj)
+    const noteobj = {
+        id: Date.now(),
+        title: finalTitle,
+        content: trimmedContent
+    };
+
+    notes.push(noteobj);
+    console.log(notes);
+
+    title.value = "";
+    notePad.value = "";
+
+    const newNote = document.createElement("div");
+    newNote.dataset.id = noteobj.id;
+    newNote.classList.add("note-card");
+
+    const heading = document.createElement("h3");
+    heading.textContent = finalTitle;
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = trimmedContent;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+
+    deleteBtn.addEventListener("click", function(){
+        const noteId = Number(newNote.dataset.id);
+        notes = notes.filter(note => note.id !== noteId);
+        newNote.remove();
+    })
+
+
+    newNote.append(heading);
+    newNote.append(paragraph);
+    newNote.append(deleteBtn);
+    notesContainer.append(newNote);
+
 })
 
